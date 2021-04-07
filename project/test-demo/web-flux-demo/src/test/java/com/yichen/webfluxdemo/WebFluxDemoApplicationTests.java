6e36a1add027486ba6644d5d6658a3c68428850c
yichen17 <q07218396@163.com>
2021-04-07 17:05:18 +0800

    feat(add module): 新增了两个模块
    algorithm-demo 模块用于写算法
    web-flux-demo 用于实现 webFlux
    今天研究了  netty
    server启动流程，初步理解，待研究。

---
+++ project/test-demo/web-flux-demo/src/test/java/com/yichen/webfluxdemo/WebFluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yichen.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
