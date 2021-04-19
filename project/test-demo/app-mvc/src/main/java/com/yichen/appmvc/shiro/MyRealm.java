bcc8613b4fec92b69a27149c05bc5b63001606c5
qiuxinchao <qiuxinchao@fbank.com.cn>
2021-04-19 17:47:30 +0800

    shiro 入门demo

---
+++ project/test-demo/app-mvc/src/main/java/com/yichen/appmvc/shiro/MyRealm.java
@@ -0,0 +1,110 @@
+package com.yichen.appmvc.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Qiuxinchao
+ * @version 1.0
+ * @date 2021/4/19 16:50
+ * @describe  realm 实现类
+ */
+public class MyRealm extends AuthorizingRealm {
+
+    /**
+     * 模拟数据库数据
+     */
+    Map<String, String> userMap = new HashMap<>(16);
+
+    {
+        userMap.put("wmyskxz", "123456");
+        super.setName("myRealm"); // 设置自定义Realm的名称，取什么无所谓..
+    }
+
+    /**
+     * 授权
+     *
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String userName = (String) principalCollection.getPrimaryPrincipal();
+        // 从数据库获取角色和权限数据
+        Set<String> roles = getRolesByUserName(userName);
+        Set<String> permissions = getPermissionsByUserName(userName);
+
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        simpleAuthorizationInfo.setStringPermissions(permissions);
+        simpleAuthorizationInfo.setRoles(roles);
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 模拟从数据库中获取权限数据
+     *
+     * @param userName
+     * @return
+     */
+    private Set<String> getPermissionsByUserName(String userName) {
+        Set<String> permissions = new HashSet<>();
+        permissions.add("user:delete");
+        permissions.add("user:add");
+        return permissions;
+    }
+
+    /**
+     * 模拟从数据库中获取角色数据
+     *
+     * @param userName
+     * @return
+     */
+    private Set<String> getRolesByUserName(String userName) {
+        Set<String> roles = new HashSet<>();
+        roles.add("admin");
+        roles.add("user");
+        return roles;
+    }
+
+    /**
+     * 认证
+     *
+     * @param authenticationToken 主体传过来的认证信息
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 1.从主体传过来的认证信息中，获得用户名
+        String userName = (String) authenticationToken.getPrincipal();
+
+        // 2.通过用户名到数据库中获取凭证
+        String password = getPasswordByUserName(userName);
+        if (password == null) {
+            return null;
+        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo("wmyskxz", password, "myRealm");
+        return authenticationInfo;
+    }
+
+    /**
+     * 模拟从数据库取凭证的过程
+     *
+     * @param userName
+     * @return
+     */
+    private String getPasswordByUserName(String userName) {
+        return userMap.get(userName);
+    }
+}
